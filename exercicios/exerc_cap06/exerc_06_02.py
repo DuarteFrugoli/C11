0941cc1c61c928d20bc5782d9e65f14fbd23009a
Pedro Henrique Duarte Frugoli <pedro.frugoli@ges.inatel.br>
2025-04-10 18:52:59 -0300

    aaahahahahahahahaaaaAHAAH

--- exercicios/exerc_cap06/exerc_06_02.py
+++ exercicios/exerc_cap06/exerc_06_02.py
@@ -2,5 +2,28 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Importando dataset
 df_space = pd.read_csv('space.csv', sep=';')
+
+# Filtra os dados para os EUA e China e remove duplicatas
+empresas_eua = df_space[df_space["Location"].str.contains("USA")]["Company Name"].drop_duplicates()
+empresas_china = df_space[df_space["Location"].str.contains("China")]["Company Name"].drop_duplicates()
+
+# Conta o número de empresas únicas
+num_empresas = {
+    "EUA": len(empresas_eua),
+    "China": len(empresas_china)
+}
+
+# Cria o gráfico de barras
+plt.figure(figsize=(8, 5))
+plt.bar(num_empresas.keys(), num_empresas.values(), color=['blue', 'red'])
+
+# Títulos e legendas
+plt.title("Número de Empresas Espaciais - EUA vs China")
+plt.xlabel("País")
+plt.ylabel("Número de Empresas")
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+# Exibe o gráfico
+plt.tight_layout()
+plt.show()
--- exercicios/exerc_cap06/exerc_06_03.py
+++ exercicios/exerc_cap06/exerc_06_03.py
@@ -4,3 +4,20 @@ import matplotlib.pyplot as plt
 
 # Importando dataset
 df_space = pd.read_csv('space.csv', sep=';')
+
+# Filtra os dados para a empresa Roscosmos
+roscosmos_missoes = df_space[df_space["Company Name"] == "Roscosmos"]
+
+# Conta o número de missões bem-sucedidas e mal-sucedidas
+status_missoes = roscosmos_missoes["Status Mission"].value_counts()
+
+# Cria o gráfico de torta
+plt.figure(figsize=(8, 8))
+plt.pie(status_missoes, labels=status_missoes.index, autopct='%1.1f%%', startangle=90, colors=['green', 'red'])
+
+# Títulos
+plt.title("Porcentagem de Sucesso e Falha das Missões da Roscosmos")
+
+# Exibe o gráfico
+plt.tight_layout()
+plt.show()
